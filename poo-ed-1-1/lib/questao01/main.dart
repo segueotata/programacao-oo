9a5ed7d45090addc23fe59fb6081215ffed3b28b
segueotata <brazmagri22@gmail.com>
2026-03-23 21:41:58 -0300

    Exercicio 01

---
+++ poo-ed-1-1/lib/questao01/main.dart
@@ -0,0 +1,14 @@
+import 'pessoa.dart';
+
+int main() {
+    Pessoa p1 = Pessoa('Braz', 22);
+    p1.exibirDados();
+    
+    Pessoa p2 = Pessoa('Jhonatan', 25);
+    p2.exibirDados();
+    
+    Pessoa p3 = Pessoa('Daniel', 99);
+    p3.exibirDados();
+
+    return 0;
+};
